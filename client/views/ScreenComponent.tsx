819ed6aaf93a4706cf2a7719ec6b4c62b00dbb5d
andreymykhaylov <pfodox1@gmail.com>
2018-04-19 13:28:57 +0300

    mobX - stack overflow

--- client/views/ScreenComponent.tsx
+++ client/views/ScreenComponent.tsx
@@ -1,239 +1,90 @@
 import * as React from "react";
 import {observer} from "mobx-react";
 import {observable} from "mobx";
-
-let A = {
-    letter: 'А',
-    imgSrc: 'avtomobil',
-    soundSrc: 'A',
-    soglasnaya: false
-};
-
-let B = {
-    letter: 'Б',
-    imgSrc: 'buldozer',
-    soundSrc: 'B',
-    soglasnaya: true
-};
-let W = {
-    letter: 'В',
-    imgSrc: 'velosiped',
-    soundSrc: 'W',
-    soglasnaya: true
-};
-let G = {
-    letter: 'Г',
-    imgSrc: 'gruzovik',
-    soundSrc: 'G',
-    soglasnaya: true
-};
-let D = {
-    letter: 'Д',
-    imgSrc: 'jeep',
-    soundSrc: 'D',
-    soglasnaya: true
-};
-let JE = {
-    letter: 'Е',
-    imgSrc: 'eraz',
-    soundSrc: 'JE',
-    soglasnaya: false
-};
-let JO = {
-    letter: 'Ё',
-    imgSrc: 'e-mobil',
-    soundSrc: 'JO',
-    soglasnaya: false
-};
-let J = {
-    letter: 'Ж',
-    imgSrc: 'jiguli',
-    soundSrc: 'J',
-    soglasnaya: true
-};
-let Z = {
-    letter: 'З',
-    imgSrc: 'znak',
-    soundSrc: 'Z',
-    soglasnaya: true
-};
-let I = {
-    letter: 'И',
-    imgSrc: 'istrebitel',
-    soundSrc: 'I',
-    soglasnaya: false
-};
-let JI = {
-    letter: 'Й',
-    imgSrc: 'tramvay',
-    soundSrc: 'JI',
-    soglasnaya: true
-};
-let K = {
-    letter: 'К',
-    imgSrc: 'korabl',
-    soundSrc: 'K',
-    soglasnaya: true
-};
-let L = {
-    letter: 'Л',
-    imgSrc: 'limuzin',
-    soundSrc: 'L',
-    soglasnaya: true
-};
-let M = {
-    letter: 'М',
-    imgSrc: 'motozikl',
-    soundSrc: 'M',
-    soglasnaya: true
-};
-let N = {
-    letter: 'Н',
-    imgSrc: 'nomer',
-    soundSrc: 'N',
-    soglasnaya: true
-};
-let O = {
-    letter: 'О',
-    imgSrc: 'omnibus',
-    soundSrc: 'O',
-    soglasnaya: false
-};
-let P = {
-    letter: 'П',
-    imgSrc: 'poezd',
-    soundSrc: 'P',
-    soglasnaya: true
-};
-let R = {
-    letter: 'Р',
-    imgSrc: 'raketa',
-    soundSrc: 'R',
-    soglasnaya: true
-};
-let S = {
-    letter: 'С',
-    imgSrc: 'samolet',
-    soundSrc: 'S',
-    soglasnaya: true
-};
-let T = {
-    letter: 'Т',
-    imgSrc: 'traktor',
-    soundSrc: 'T',
-    soglasnaya: true
-};
-let U = {
-    letter: 'У',
-    imgSrc: 'uprjagka',
-    soundSrc: 'U',
-    soglasnaya: false
-};
-let F = {
-    letter: 'Ф',
-    imgSrc: 'fura',
-    soundSrc: 'F',
-    soglasnaya: true
-};
-let H = {
-    letter: 'Х',
-    imgSrc: 'hlebovoz',
-    soundSrc: 'H',
-    soglasnaya: true
-};
-let C = {
-    letter: 'Ц',
-    imgSrc: 'zementovoz',
-    soundSrc: 'C',
-    soglasnaya: true
-};
-let CH = {
-    letter: 'Ч',
-    imgSrc: 'cheln',
-    soundSrc: 'CH',
-    soglasnaya: true
-};
-let SH = {
-    letter: 'Ш',
-    imgSrc: 'shofer',
-    soundSrc: 'SH',
-    soglasnaya: true
-};
-let SHJ = {
-    letter: 'Щ',
-    imgSrc: 'schetka',
-    soundSrc: 'SHJ',
-    soglasnaya: true
-};
-let TV = {
-    letter: 'Ъ',
-    imgSrc: 'ob\'ezd',
-    soundSrc: 'TV',
-    soglasnaya: true
-};
-let Y = {
-    letter: 'Ы',
-    imgSrc: 'fary',
-    soundSrc: 'Y',
-    soglasnaya: false
-};
-let MJA = {
-    letter: 'Ь',
-    imgSrc: 'rul\'',
-    soundSrc: 'MJA',
-    soglasnaya: true
-};
-let E = {
-    letter: 'Э',
-    imgSrc: 'evakuator',
-    soundSrc: 'E',
-    soglasnaya: false
-};
-let JU = {
-    letter: 'Ю',
-    imgSrc: 'junga',
-    soundSrc: 'JU',
-    soglasnaya: false
-};
-let JA = {
-    letter: 'Я',
-    imgSrc: 'jahta',
-    soundSrc: 'JA',
-    soglasnaya: false
-};
-let Letters = [A, B, W, G, D, JE, JO, J, Z, I, JI, K, L, M, N, O, P, R, S, T, U, F, H, C, CH, SH, SHJ, TV, Y, MJA, E, JU, JA];
+import * as cx from "classnames";
 
 @observer
-export class Screen extends React.Component<{}, {}> {
-
-    @observable audio: any;
-    play = (sound: string) => {
-        if (this.audio) {
-            this.audio.pause();
-            this.audio.currentTime = 0;
-        }
-        let newAudio = new Audio(`/resources/audio/${sound}.mp3`);
-        newAudio.play();
-        this.audio = newAudio;
+export class Screen extends React.Component {
+    @observable input1;
+    state = {
+        firstNumber: false,
+        secondNumber: false,
+        numberA: undefined,
+        numberB: undefined,
+        numberC: undefined,
     };
 
-    render() {
+    inputOnChange = () => {
+        if (this.input1.value == this.state.numberA) {
+            this.setState({firstNumber: true})
+        }
+        if(this.input1.value != this.state.numberA){
 
-        const ABC = Letters.map((item, index) => (
-            <div onClick={() => {
-                this.play(item.soundSrc)
-            }}
-                 onDoubleClick={() => {
-                     item.soundSrc == "TV" || item.soundSrc == "Y" || item.soundSrc == "MJA" ? window.location.href = `/` : window.location.href = `/letter/${item.soundSrc}`
-                 }}>
-                <span className={item.soglasnaya ? 'blue' : 'red'}>{item.letter}</span>
-                <img src={`../resources/images/${item.imgSrc}.jpg`}/>
-            </div>
-        ));
+        }
+    };
+    randomInteger = (min, max) => {
+        var rand = min + Math.random() * (max + 1 - min);
+        rand = Math.floor(rand);
+        return rand;
+    };
+    componentWillMount() {
+        const a = this.randomInteger(6, 9);
+        const c = this.randomInteger(11, 14);
+        const b = c - a;
+        this.setState({numberA: a, numberB: b, numberC: c})
+    }
+
+    render() {
+        const {numberA, numberB, numberC} = this.state;
+        const inputClasses = cx ({
+            red: !this.state.firstNumber
+        });
+        // const aClasses =  cx ("a", {
+        //     yellow: this.input1 && this.input1.value != "" && !this.state.firstNumber
+        // });
+        if (this.input1)console.log(this.state.firstNumber, this.input1.value);
         return (
-            <div className="display">
-                {ABC}
+            <div className="card">
+                <div className="task">
+                    <div className="a">{numberA}</div>
+                    <div>+</div>
+                    <div className="b">{numberB}</div>
+                    <div>=</div>
+                    <div className="c">{numberC}</div>
+                </div>
+                <div className="pictures">
+                    <div>
+                        <div className="arcA">
+                            <div className="inputA input" style={{marginLeft: `${13.13 * numberA - 8}px`}}>
+                                <input
+                                    type="number"
+                                    className={inputClasses}
+                                    ref={(input) => {
+                                        this.input1 = input;
+                                    }}
+                                    onChange={this.inputOnChange}/>
+                            </div>
+                            <div className="arc"
+                                 style={{width: `${26.26 * numberA}px`, height: `${13.13 * numberA}px`}}></div>
+                            <div className="arrow down"></div>
+                        </div>
+                        <If condition={this.state.firstNumber}>
+                            <div className="arcB">
+                                <div className="inputB input" style={{marginLeft: `${13.13 * numberB - 8}px`}}>
+                                    <input type="number"/>
+                                </div>
+                                <div className="arc" style={{
+                                    width: `${26.26 * numberB}px`,
+                                    height: `${13.13 * numberB}px`
+                                }}></div>
+                                <div className="arrow down"></div>
+                            </div>
+                        </If>
+                    </div>
+                    <div className="ruler"><img src=".\resources\images\sprite.png"/></div>
+                </div>
             </div>
         )
     }
 }
+
