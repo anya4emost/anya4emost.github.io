bf0040c9421e3157d4320cbaca30c606e9e21045
annaMikhaylova <pfodox1@gmail.com>
2018-04-20 13:10:56 +0300

    prod

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -3,9 +3,8 @@ const webpackConfigPath = {
     'production': './webpack/prod',
     'githubPages': './webpack/githubPages'
 };
-console.log(process.env.NODE_ENV);
-//process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-process.env.NODE_ENV = "production"
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 const path = webpackConfigPath[process.env.NODE_ENV];
 
 module.exports = require(path);
